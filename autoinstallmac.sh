a69369d984aebb9f79e9c9a8371dffdc6bd45fd7
hvidinhas <hvidinhas@icloud.com>
2023-08-17 16:53:29 -0300

    removi libreoffice e coloquei no lugar o onlyoffice na lista do brew

--- autoinstallmac.sh
+++ autoinstallmac.sh
@@ -46,7 +46,7 @@ brew install neovim;
 brew install node;
 brew install --cask notion;
 brew install --cask onyx;
-brew install --cask libreoffice;
+brew install --cask onlyoffice;
 brew install rclone;
 brew install rust;
 brew install speedtest --force;
