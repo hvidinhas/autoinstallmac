2136016ada9d2c9f5333f676707bfb1407c3345b
Heitor <hvidinhas@icloud.com>
2025-02-05 14:31:15 -0300

    adicionei o comando para adicionar um script de desligar monitor na pasta USR do mac

--- autoinstallmac.sh
+++ autoinstallmac.sh
@@ -139,9 +139,11 @@ defaults write com.apple.TextEdit NSShowAppCentricOpenPanelInsteadOfUntitledFile
 sudo ln -s ~/Documents/Codes/Backup-Mac/backupmac.sh /usr/local/bin/backup;
 sudo ln -s ~/Documents/Codes/Scripts/legenda.sh /usr/local/bin/legenda;
 sudo ln -s ~/Documents/Codes/Scripts/limpasd.sh /usr/local/bin/limpaSD;
+sudo ln -s ~/Documents/Codes/Scripts/horadedormir.sh /usr/local/bin/horadedormir;
 sudo chmod -v 0775 /usr/local/bin/backup;
 sudo chmod -v 0775 /usr/local/bin/legenda;
 sudo chmod -v 0775 /usr/local/bin/limpaSD;
+sudo chmod -v 0775 /usr/local/bin/horadedormir;
 
 # ------------- Copiando preferencias ---------------------- #
 cp -vrf ~/Documents/Arquivos/Application\ Support/ ~/Library/Application\ Support;
