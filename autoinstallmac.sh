34031177cfdfd695c36f2ea612b91541c55accc6
hvidinhas <hvidinhas@icloud.com>
2025-05-07 19:22:01 -0300

    Modifiquei os caminhos dos links simbólicos, para nova pasta do Google Drive

--- autoinstallmac.sh
+++ autoinstallmac.sh
@@ -137,13 +137,11 @@ defaults write com.apple.TextEdit NSShowAppCentricOpenPanelInsteadOfUntitledFile
 
 
 # --------------- Criando links símbolicos --------------- #
-sudo ln -s ~/Documents/Codes/Backup-Mac/backupmac.sh /usr/local/bin/backup;
-sudo ln -s ~/Documents/Codes/Scripts/legenda.sh /usr/local/bin/legenda;
-sudo ln -s ~/Documents/Codes/Scripts/limpasd.sh /usr/local/bin/limpaSD;
-sudo ln -s ~/Documents/Codes/Scripts/horadedormir.sh /usr/local/bin/horadedormir;
+sudo ln -s ~/Google\ Drive/My\ Drive/Codes/Backup-Mac/backupmac.sh /usr/local/bin/backup;
+sudo ln -s ~/Google\ Drive/My\ Drive/Codes/Scripts/legenda.sh /usr/local/bin/legenda;
+sudo ln -s ~/Google\ Drive/My\ Drive/Codes/Scripts/horadedormir.sh /usr/local/bin/horadedormir;
 sudo chmod -v 0775 /usr/local/bin/backup;
 sudo chmod -v 0775 /usr/local/bin/legenda;
-sudo chmod -v 0775 /usr/local/bin/limpaSD;
 sudo chmod -v 0775 /usr/local/bin/horadedormir;
 
 # ------------- Copiando preferencias ---------------------- #
